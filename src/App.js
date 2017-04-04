1acff604d6b69b4287facbafbd4cfa4d1c1a9cd8
Diego Cáceres <dcaceres@xseed.com.uy>
2017-04-04 14:29:36 -0300

    Separating the first component TodoForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoForm} from './components/TodoForm'
 
 class App extends Component {
   constructor(props){
@@ -29,9 +30,8 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
-          <form>
-            <input type="text" value={this.state.currentTodo} onChange={this.handleInputChange} />
-          </form>
+          <TodoForm handleInputChange={this.handleInputChange}
+                    currentTodo={this.state.currentTodo} />
           <div>
             <ul>
               {this.state.todos.map(todo => 
