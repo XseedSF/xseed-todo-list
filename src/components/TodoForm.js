146c44a841bb36d34ff1185b18e9cdec41c794cf
Diego Cáceres <dcaceres@xseed.com.uy>
2017-04-04 14:54:30 -0300

    Adding some PropTypes to validate props in components

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const TodoForm = (props) => (
 	<form>
-		<input type="text" value={props.currentTodo} onChange={props.handleInputChange} />
+		<input type="text" 
+		value={props.currentTodo} 
+		onChange={props.handleInputChange} />
 	</form>
 )
+
+TodoForm.propTypes = {
+	currentTodo: React.PropTypes.string.isRequired,
+	handleInputChange: React.PropTypes.func.isRequired
+}
