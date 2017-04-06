1316fa5e102be36413b0947e9f3d48a55b9f3a86
Diego Cáceres <dado90caceres@gmail.com>
2017-04-06 00:47:28 -0300

    removed the empty submit functionality in order to clean the state away. Will be added properly later

--- src/App.js
+++ src/App.js
@@ -7,21 +7,9 @@ import TodoList from './components/TodoList'
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      
-    }
-    this.handleEmptySubmit = this.handleEmptySubmit.bind(this);
-  }
-
-  handleEmptySubmit(event) {
-    event.preventDefault();
-    this.setState({
-      errorMessage: 'Please supply a todo name'
-    })
   }
 
   render() {
-    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
     return (
       <div className="App">
         <div className="App-header">
@@ -29,7 +17,6 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
-          {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
           <TodoForm />          
           <TodoList />
         </div>
